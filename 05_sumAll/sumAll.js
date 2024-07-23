b4ec546fc3ff3b7b6a4a2e7ced1e7c3ac434c3cc
maciejthewhitehat <maciek.chitrosz@gmail.com>
2024-07-23 16:51:06 +0200

    Added solution to sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    sum = 0;
+    if(Number.isInteger(num1) && Number.isInteger(num2)) {
+        if(num1 < 0 || num2 < 0) {
+            return "ERROR";
+        }
 
+        if(num1 < num2) {
+            for(i = num1; i <= num2; i++) {
+                sum += i;
+            }
+        }
+        else {
+            if(num1 > num2) {
+                for(i = num2; i <= num1; i++) {
+                    sum += i;
+                }
+            }
+        }
+        return sum;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
