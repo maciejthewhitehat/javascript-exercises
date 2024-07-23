d037c27685b762ef35f3a8d29a1abce7da552580
maciejthewhitehat <maciek.chitrosz@gmail.com>
2024-07-23 17:40:02 +0200

    Added solution to calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,61 @@
-const add = function() {
-	
+const add = function(addNum1, addNum2) {
+	addResult = addNum1 + addNum2;
+  return addResult;
 };
 
-const subtract = function() {
-	
+const subtract = function(substractNum1, substractNum2) {
+	substractResult = substractNum1 - substractNum2;
+  return substractResult;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+  sumResult = 0;
+
+	for(i = 0; i < sumArray.length; i++) {
+    sumResult += sumArray[i];
+  }
+
+  return sumResult;
 };
 
-const multiply = function() {
+const multiply = function(multiplyArray) {
+  multiplyResult = 1;
 
+  for(j = 0; j < multiplyArray.length; j++) {
+    multiplyResult = multiplyResult * multiplyArray[j];
+  }
+
+  return multiplyResult;
 };
 
-const power = function() {
-	
+const power = function(powerNumber, powerValue) {
+	powerResult = 1;
+
+  for(k = 1; k <= powerValue; k++) {
+    powerResult = powerResult * powerNumber;
+  }
+  
+  return powerResult;
 };
 
-const factorial = function() {
-	
+const factorial = function(factorialNumber) {
+  if(factorialNumber < 0) {
+    return "ERROR";
+  }
+  else {
+    if(factorialNumber == 0) {
+      factorialResult = 1;
+    }
+    else {
+      factorialResult = 1;
+
+      for(l = 1; l <= factorialNumber; l++) {
+        factorialResult = factorialResult * l;
+      }
+    }
+    
+    return factorialResult;
+  }
 };
 
 // Do not edit below this line
