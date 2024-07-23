885400a2bbf7c1493cf71f0dd9ebd91cf80ac610
maciejthewhitehat <maciek.chitrosz@gmail.com>
2024-07-23 17:02:46 +0200

    Added solution in leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,25 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(Number.isInteger(year)) {
+        if(Number.isInteger(year / 4)) {
+            if(Number.isInteger(year / 100) && Number.isInteger(year / 400)) {
+                return true;
+            }
+            else {
+                if(Number.isInteger(year / 100) && !(Number.isInteger(year / 400))) {
+                return false;
+                }
+                else {
+                    return true;
+                }
+            }
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
