52b1ef4bc73f6f85392d08702301c27d5826fc4b
maciejthewhitehat <maciek.chitrosz@gmail.com>
2024-07-23 16:37:59 +0200

    Added solution in reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+let i,
+    array,
+    string;
 
+const reverseString = function(string) {
+    array = [];
+    for(i = string.length - 1; i >= 0; i--) {
+        array.push(string.charAt(i));
+    }
+    result = array.join("");
+    return result;
 };
 
 // Do not edit below this line
