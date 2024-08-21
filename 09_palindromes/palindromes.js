fe8930acd731853410beaff6842248556d1d8f63
maciejthewhitehat <maciek.chitrosz@gmail.com>
2024-08-21 10:10:00 +0200

    Added solution to palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const characterTable =  [];
+    let j = 0;
+    for(i = 0; i < string.length; i++) {
+        if(string.charAt(i).match(/[A-Za-z0-9]/)) {
+            if(string.charAt(i).match(/[A-Za-z]/)) {
+                tempChar = string.charAt(i);
+                lowercaseTempChar = tempChar.toLowerCase();
+                characterTable[j] = lowercaseTempChar;
+            }
+            else {
+                characterTable[j] = string.charAt(i);
+            }
+        j++;
+        }
+    }
+    for(k = 0; k < characterTable.length; k++) {
+        if(characterTable[k] !== characterTable[characterTable.length - k - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
