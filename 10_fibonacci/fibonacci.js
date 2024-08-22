089ffa496c463984a27def2c35cbd9deab668f47
maciejthewhitehat <maciek.chitrosz@gmail.com>
2024-08-22 08:19:30 +0200

    Added solution to fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number >= 0) {
+        const fibonacciTable = [];
+        fibonacciTable[0] = 0;
+        fibonacciTable[1] = 1;
+        fibonacciTable[2] = 1;
+        for(i = 2; i <= number; i++) {
+            fibonacciTable[i] = fibonacciTable[i-1] + fibonacciTable[i-2];
+        }
+        return fibonacciTable[number];
+    }
+    else return "OOPS";
 };
 
 // Do not edit below this line
