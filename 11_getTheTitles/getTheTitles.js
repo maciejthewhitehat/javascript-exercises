7f6b54efbad1c25956e3ad9c6930467e94ec821b
maciejthewhitehat <maciek.chitrosz@gmail.com>
2024-08-22 08:28:06 +0200

    Added solution to getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(givenArray) {
+    titlesArray = givenArray.map((book => book.title));
+    return titlesArray;
 };
 
 // Do not edit below this line
